107b9e28d440e6fd71e9edcb73de7279bd003f70
javierlm <jlmedina.tfe.92@gmail.com>
2020-05-10 14:32:27 +0100

    Changed search order on Spotify to improve the results

--- create_playlist.py
+++ create_playlist.py
@@ -160,9 +160,9 @@ class CreatePlaylist:
 
     def get_spotify_uri(self, song_name, artist):
         """Search For the Song"""
-        query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
-            song_name,
-            artist
+        query = "https://api.spotify.com/v1/search?query=artist%3A{}+track%3A{}&type=track&offset=0&limit=20".format(
+            artist,
+            song_name
         )
         response = requests.get(
             query,
